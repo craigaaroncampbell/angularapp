59d410a9dae18090106a59ebb20ffa1dc31c1c89
Craig Campbell <craig.campbell8@gmail.com>
2015-11-15 14:57:26 -0800

    done with authentication

--- routes/beers_routes.js
+++ routes/beers_routes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var Beer = require(__dirname + '/../models/beer.js');
-
+var eatAuth = require(__dirname + '/../lib/eat_auth.js');
 var handleError = require(__dirname + '/../lib/handleErrors.js');
 var beersRouter = module.exports = exports = express.Router();
 
@@ -12,7 +12,7 @@ beersRouter.get('/beers', function(req, res) {
   });
 });
 
-beersRouter.post('/beers', bodyParser.json(), function(req, res) {
+beersRouter.post('/beers', bodyParser.json(), eatAuth, function(req, res) {
   var newBeer = new Beer(req.body);
   newBeer.save(function(err, data) {
     if (err) handleError(err, res);
@@ -20,7 +20,7 @@ beersRouter.post('/beers', bodyParser.json(), function(req, res) {
   });
 });
 
-beersRouter.put('/beers/:id', bodyParser.json(), function(req, res) {
+beersRouter.put('/beers/:id', bodyParser.json(), eatAuth, function(req, res) {
   var beerData = req.body;
   delete beerData._id;
   Beer.update({_id: req.params.id}, beerData, function(err) {
