e9e9fa413be47a8fe949af24c00a6764f4ea6d3f
Craig Campbell <craig.campbell8@gmail.com>
2015-11-14 16:54:38 -0800

    finished jscs styling

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,20 @@ var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var stylish = require('gulp-jscs-stylish');
 
-
 gulp.task('default', ['lint', 'jscs', 'test']);
 
-gulp.task('lint', function(){
+gulp.task('lint', function() {
   gulp.src(['gulpfile.js', 'server.js', 'models/**/*.js', 'test/**/*test.js', 'lib/**/*.js', 'routes/**/*.js'])
   .pipe(jshint())
   .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('test', ['lint'], function(){
+gulp.task('test', ['lint'], function() {
   gulp.src('test/**/*test.js')
   .pipe(mocha({reporter: 'nyan'}));
 });
 
-gulp.task('jscs', function(){
+gulp.task('jscs', function() {
   gulp.src(['gulpfile.js', 'server.js', 'models/**/*.js', 'test/**/*test.js', 'lib/**/*.js', 'routes/**/*.js'])
   .pipe(jscs())
   .pipe(stylish());
--- routes/beers_routes.js
+++ routes/beers_routes.js
@@ -5,41 +5,41 @@ var Beer = require(__dirname + '/../models/beer.js');
 var handleError = require(__dirname + '/../lib/handleErrors.js');
 var beersRouter = module.exports = exports = express.Router();
 
-beersRouter.get('/beers', function(req,res){
-  Beer.find({}, function(err, data){
+beersRouter.get('/beers', function(req, res) {
+  Beer.find({}, function(err, data) {
     if (err) handleError(err, res);
     res.send(data);
   });
 });
 
-beersRouter.post('/beers', bodyParser.json(), function(req, res){
+beersRouter.post('/beers', bodyParser.json(), function(req, res) {
   var newBeer = new Beer(req.body);
-  newBeer.save(function(err, data){
+  newBeer.save(function(err, data) {
     if (err) handleError(err, res);
     res.send(data);
   });
 });
 
-beersRouter.put('/beers/:id', bodyParser.json(), function(req, res){
+beersRouter.put('/beers/:id', bodyParser.json(), function(req, res) {
   var beerData = req.body;
   delete beerData._id;
-  Beer.update({_id: req.params.id}, beerData, function(err){
+  Beer.update({_id: req.params.id}, beerData, function(err) {
     if (err) return handleError(err, res);
     console.log(req.body);
     res.send({msg: 'updated!'});
   });
 });
 
-beersRouter.delete('/beers/:id', function(req, res){
-  Beer.remove({_id: req.params.id}, function(err){
+beersRouter.delete('/beers/:id', function(req, res) {
+  Beer.remove({_id: req.params.id}, function(err) {
     if (err) return handleError(err, res);
     res.send({msg: 'deleted!'});
   });
 });
 
-beersRouter.get('/beers/:brewery', function(req, res){
+beersRouter.get('/beers/:brewery', function(req, res) {
   /// get route that that will search the DB  by brewery name, and pull up all the beers for that brewery
-  Beer.find({brewery: req.params.brewery}, function(err, data){
+  Beer.find({brewery: req.params.brewery}, function(err, data) {
     if (err) return handleError(err, res);
     res.send(data);
   });
