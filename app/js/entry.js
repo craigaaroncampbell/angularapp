3f15bf2110fd1befa63956f138b58c9125a53ee2
Craig Campbell <craig.campbell8@gmail.com>
2015-11-24 13:06:57 -0800

    more angular

--- app/js/entry.js
+++ app/js/entry.js
@@ -1,8 +1,8 @@
 require('angular/angular');
 var angular = window.angular; //so jshint will stop complaining
 
-var bearApp = angular.module('bearstream', [] ); //empty array will hold dependencies for angular
+var bearApp = angular.module('beerstream', [] ); //empty array will hold dependencies for angular
 
 bearApp.controller('GreetingController', ['$scope', function($scope) {
-	$scope.greeting = 'This app allows users to sign in and write a log of the beers he/she has tried';
+	$scope.greeting = 'This app allows users to sign in and write a log of the beers he/she has tried. At least it should when it is all done and working.';
 }]);
