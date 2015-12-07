48972ccf145b7a5ad329c2372ee2a8905ad7ef27
Craig Campbell <craig.campbell8@gmail.com>
2015-12-07 13:59:32 -0800

    add angular-route and cookies dependency files

--- app/js/entry.js
+++ app/js/entry.js
@@ -1,6 +1,7 @@
 require('angular/angular');
+require('angular-route');
 var angular = window.angular;
 
-var beerApp = angular.module('beerApp', []);
+var beerApp = angular.module('beerApp', ['ngRoute']);
 
 require('./beers/beers.js')(beerApp);
