886a6729374438789232aa45cb06ccf2bf84dd38
Craig Campbell <craig.campbell8@gmail.com>
2015-12-08 21:17:41 -0800

    first commit

--- app/js/beers/directives/beers_directives.js
+++ app/js/beers/directives/beers_directives.js
@@ -1,6 +1,7 @@
 module.exports = function(app) {
 	app.directive('beerforms', function() {
 		return {
+			restrict: 'AC',
 			templateUrl: 'templates/beerforms.html',
 			controller: 'BeersController',
 			replace: true
--- test/client/test_bundle.js
+++ test/client/test_bundle.js
@@ -30329,6 +30329,7 @@
 	module.exports = function(app) {
 		app.directive('beerforms', function() {
 			return {
+				// restrict: 'AC',
 				templateUrl: 'templates/beerforms.html',
 				controller: 'BeersController',
 				replace: true
